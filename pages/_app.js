caf8718340419073142fe285e7963306eb053bdb
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2021-08-20 16:28:30 +0000

    add auth0, faunadb, chakra-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { user } = pageProps;
+
+  return (
+    <UserProvider user={user}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
