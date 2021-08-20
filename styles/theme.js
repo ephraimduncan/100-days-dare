4513b92b0f2239e17115eba22a83392a8d3eba57
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2021-08-20 18:08:09 +0000

    added custom colors to theme.js for chakra

---
+++ styles/theme.js
@@ -0,0 +1,18 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    blue: "#5A06FF",
+    ash: "#E1D9F2",
+    orange: "#FFAC06",
+    gray: {
+      100: "#F5EBD6",
+      200: "#F3E2ED",
+    },
+    lime: "#ACFF06",
+    black: "#323232",
+    pink: "#FF06AC",
+  },
+});
+
+export default theme;
