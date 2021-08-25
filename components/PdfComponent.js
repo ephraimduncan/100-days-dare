b87f90588325285218b03c4c7357c94e7812403c
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2021-08-25 15:03:15 +0000

    ✨ feat: generate pdf based of users data

---
+++ components/PdfComponent.js
@@ -0,0 +1,180 @@
+import {
+  PDFDownloadLink,
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+} from "@react-pdf/renderer";
+import { Text as ChakraText } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+// Create styles
+const BORDER_COLOR = "#4A5568";
+const BORDER_STYLE = "solid";
+const BORDER_WIDTH = 2;
+const COL1_WIDTH = 12;
+const COLN_WIDTH = (100 - COL1_WIDTH) / 2;
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "#fff",
+    padding: 10,
+  },
+  imageClass: {
+    width: "50%",
+    alignSelf: "center",
+  },
+  section: {
+    margin: 5,
+  },
+  table: {
+    margin: 20,
+    display: "table",
+    width: "auto",
+    borderStyle: BORDER_STYLE,
+    borderColor: BORDER_COLOR,
+    borderWidth: BORDER_WIDTH,
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+  },
+  tableRow: {
+    margin: "auto",
+    flexDirection: "row",
+  },
+  tableCol1Header: {
+    width: COL1_WIDTH + "%",
+    borderStyle: BORDER_STYLE,
+    borderColor: BORDER_COLOR,
+    borderBottomColor: "#323232",
+    borderWidth: BORDER_WIDTH,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+  },
+  tableColHeader: {
+    width: COLN_WIDTH + "%",
+    borderStyle: BORDER_STYLE,
+    borderColor: BORDER_COLOR,
+    borderBottomColor: "#323232",
+    borderWidth: BORDER_WIDTH,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+  },
+  tableCol1: {
+    width: COL1_WIDTH + "%",
+    borderStyle: BORDER_STYLE,
+    borderColor: BORDER_COLOR,
+    borderWidth: BORDER_WIDTH,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+  },
+  tableCol: {
+    width: COLN_WIDTH + "%",
+    borderStyle: BORDER_STYLE,
+    borderColor: BORDER_COLOR,
+    borderWidth: BORDER_WIDTH,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+  },
+  tableCellHeader: {
+    margin: 5,
+    padding: 5,
+    fontSize: 18,
+    fontWeight: 500,
+  },
+  tableCell: {
+    margin: 5,
+    fontSize: 11,
+  },
+});
+
+// Create Document Component
+const MyDocument = ({ userData }) => {
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <Image
+          style={styles.imageClass}
+          src="./social-media-cover.png"
+        />
+        {/* Display Goal and Start Before the table */}
+        {/* Table View */}
+        <View style={styles.table}>
+          <View style={styles.tableRow}>
+            <View style={styles.tableCol1Header}>
+              <Text style={styles.tableCellHeader}>Day</Text>
+            </View>
+            <View style={styles.tableColHeader}>
+              <Text style={styles.tableCellHeader}>Task</Text>
+            </View>
+            <View style={styles.tableColHeader}>
+              <Text style={styles.tableCellHeader}>Description</Text>
+            </View>
+          </View>
+          {userData &&
+            userData.map((userData) => {
+              const DAY = Object.keys(userData)[0];
+              const TASK_TODAY = userData[DAY.toString()].taskToday;
+              const DETAILS = userData[DAY.toString()].details;
+
+              return (
+                <View style={styles.tableRow}>
+                  <View style={styles.tableCol1}>
+                    <Text style={styles.tableCell}>{DAY}</Text>
+                  </View>
+                  <View style={styles.tableCol}>
+                    <Text style={styles.tableCell}>{TASK_TODAY}</Text>
+                  </View>
+                  <View style={styles.tableCol}>
+                    <Text style={styles.tableCell}>{DETAILS}</Text>
+                  </View>
+                </View>
+              );
+            })}
+        </View>
+      </Page>
+    </Document>
+  );
+};
+
+export default function PDFApp() {
+  const [isClient, setIsClient] = useState(false);
+
+  const { data } = useSWR("/api/getUser");
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <>
+      {isClient && (
+        <PDFDownloadLink
+          document={<MyDocument userData={data && data.user.days} />}
+          fileName="100-Days-Dare.pdf"
+        >
+          {({ blob, url, loading, error }) =>
+            loading ? (
+              <TextComponent text="Loading document" />
+            ) : (
+              <TextComponent text="Export As PDF" />
+            )
+          }
+        </PDFDownloadLink>
+      )}
+    </>
+  );
+}
+
+function TextComponent({ text }) {
+  return (
+    <ChakraText
+      fontFamily="bungee"
+      fontWeight="light"
+      color="#5a06ff"
+    >
+      {text}
+    </ChakraText>
+  );
+}
